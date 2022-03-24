4f94c76473332e4b90b7d69e862088dec9cb57f4
jaideep11061982 <38164196+jaideep11061982@users.noreply.github.com>
2022-03-24 22:13:20 +0530

    Update parallel_pandas.py

--- parallel_pandas.py
+++ parallel_pandas.py
@@ -1,4 +1,5 @@
 from multiprocessing import  Pool
+from joblib import Parallel, delayed
 def add_features(df):
     #df['question_text'] = df['question_text'].apply(lambda x:str(x))
     vocab['new_words']=vocab.raw_words.progress_apply(lambda x:spell(x) )
@@ -12,3 +13,10 @@ def parallelize_dataframe(df, func, n_cores=6):
     return df
 
 train = parallelize_dataframe(vocab, add_features)
+
+def imgresize(path):
+    img=cv2.imread(path)
+    img=cv2.resize(img,(img.shape[1]//2,img.shape[0]//2))
+    img.save('/path',img)
+    
+Parallel(n_jobs=16,backend='multiprocessing')(delayed(imgresize)(path) for path in tqdm(paths))
